efef1221ae84c650f519f13af1351277ae11e2f4
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-09-22 14:09:34 -0700

    I forgot my commits commit

--- src/BstSearch.java
+++ src/BstSearch.java
@@ -23,6 +23,20 @@ public class BstSearch {
      */
     public static <T extends Comparable<T>> boolean contains(BinaryTreeNode<T> root, T target) {
         // Implement here, and make sure to implement tests too!
-        return false;
+
+        if (target == null) throw new NullPointerException("Target cannot be null.");
+
+        if (root == null) return false;
+        
+        if (target.compareTo(root.data) < 0) {
+            boolean isLeft = contains(root.left, target);
+            return isLeft;
+        } else if (target.compareTo(root.data) > 0) {
+            boolean isRight = contains(root.right, target);
+            return isRight;
+        } else {
+            return true;
+        }     
+
     }
 }
--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -3,4 +3,114 @@ import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
 
+    @Test 
+    void TestTrueScenarioRoot() {
+        BinaryTreeNode root = new BinaryTreeNode<Integer>(25);
+        BinaryTreeNode lefty = new BinaryTreeNode<Integer>(15);
+        BinaryTreeNode righty = new BinaryTreeNode<Integer>(35);
+
+        root.left = lefty;
+        root.right = righty;
+
+        assertEquals(true, BstSearch.contains(root, 25));
+    }
+
+    @Test 
+    void TestTrueScenarioLeftOne() {
+        BinaryTreeNode root = new BinaryTreeNode<Integer>(25);
+        BinaryTreeNode lefty = new BinaryTreeNode<Integer>(15);
+        BinaryTreeNode righty = new BinaryTreeNode<Integer>(35);
+
+        root.left = lefty;
+        root.right = righty;
+
+        assertEquals(true, BstSearch.contains(root, 15));
+    }
+
+    @Test void TestTrueScenarioRightOne() {
+        BinaryTreeNode root = new BinaryTreeNode<Integer>(25);
+        BinaryTreeNode lefty = new BinaryTreeNode<Integer>(15);
+        BinaryTreeNode righty = new BinaryTreeNode<Integer>(35);
+
+        root.left = lefty;
+        root.right = righty;
+
+        assertEquals(true, BstSearch.contains(root, 35));
+    }
+
+    @Test 
+    void TestTrueScenarioMultiLeft() {
+        BinaryTreeNode root = new BinaryTreeNode<Integer>(25);
+        BinaryTreeNode lefty = new BinaryTreeNode<Integer>(15);
+        BinaryTreeNode righty = new BinaryTreeNode<Integer>(35);
+        BinaryTreeNode lefty2 = new BinaryTreeNode<Integer>(14);
+        BinaryTreeNode lefty3 = new BinaryTreeNode<Integer>(16);
+
+        lefty.left = lefty2;
+        lefty.right = lefty3;
+
+        root.left = lefty;
+        root.right = righty;
+
+        assertEquals(true, BstSearch.contains(root, 16));
+        assertEquals(true, BstSearch.contains(root, 14));
+    }
+
+    @Test 
+    void TestTrueScenarioMultiRight() {
+        BinaryTreeNode root = new BinaryTreeNode<Integer>(25);
+        BinaryTreeNode lefty = new BinaryTreeNode<Integer>(15);
+        BinaryTreeNode righty = new BinaryTreeNode<Integer>(35);
+        BinaryTreeNode righty2 = new BinaryTreeNode<Integer>(34);
+        BinaryTreeNode righty3 = new BinaryTreeNode<Integer>(36);
+
+        righty.left = righty2;
+        righty.right = righty3;
+
+        root.left = lefty;
+        root.right = righty;
+
+        assertEquals(true, BstSearch.contains(root, 34));
+        assertEquals(true, BstSearch.contains(root, 36));
+    }
+
+    @Test
+    void TestFalseScenarioRoot() {
+        BinaryTreeNode root = new BinaryTreeNode<Integer>(25);
+
+        assertEquals(false, BstSearch.contains(root, 99));
+    }
+
+    @Test
+    void TestFalseScenarioTwoChildren() {
+        BinaryTreeNode root = new BinaryTreeNode<Integer>(25);
+        BinaryTreeNode lefty = new BinaryTreeNode<Integer>(15);
+        BinaryTreeNode righty = new BinaryTreeNode<Integer>(35);
+
+        root.left = lefty;
+        root.right = righty;
+
+        assertEquals(false, BstSearch.contains(root, 99));
+    }
+
+
+    @Test
+    void TestNullScenarioTwoChildren() {
+        BinaryTreeNode root = new BinaryTreeNode<Integer>(25);
+        BinaryTreeNode lefty = new BinaryTreeNode<Integer>(15);
+        BinaryTreeNode righty = new BinaryTreeNode<Integer>(35);
+
+        root.left = lefty;
+        root.right = righty;
+
+        assertThrows(NullPointerException.class, () -> BstSearch.contains(root, null));
+    }
+
+     @Test
+    void TestEmptyTree() {
+        BinaryTreeNode root = null;
+
+        assertEquals(false, BstSearch.contains(root, 5));
+    }
+    
 }
