1b92371ebeb6eef0337ec130abaf4971801945f2
sabinMas <masonsabin@gmail.com>
2026-09-23 14:37:27 -0700

    fix tests and testTreee to actually work

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -4,14 +4,19 @@ import org.junit.jupiter.api.Test;
 public class BstSearchTest {
     @Test 
     public void testNullTree() {
-        assertFalse(BstSearch.contains(null, 10));
+        assertFalse(BstSearch.contains(null, 23));
+    }
+
+    @Test 
+    public void testTreeFor13() {
+        assertTrue(BstSearch.contains(testTree(),23));
     }
 
 
 
 private BinaryTreeNode<Integer> testTree() {
-    BinaryTreeNode<Integer> root = new BinaryTreeNode<>(23);
-    root.left = new BinaryTreeNode<>(13);
+    BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(23);
+    root.left = new BinaryTreeNode<Integer>(13);
     root.left.left = new BinaryTreeNode<Integer>(7);
     root.left.right = new BinaryTreeNode<Integer>(15);
     
