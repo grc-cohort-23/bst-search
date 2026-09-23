74bc3b46f552c94eee5b5abd3c57ff89b7059f74
sabinMas <masonsabin@gmail.com>
2026-09-23 08:30:06 -0700

    add testTree to the BstSearchTest.java

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,5 +2,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+private BinaryTreeNode<Integer> testTree() {
+    BinaryTreeNode<Integer> root = new BinaryTreeNode<>(23);
+    root.left = new BinaryTreeNode<>(13);
+    root.left.left = new BinaryTreeNode<Integer>(7);
+    root.left.right = new BinaryTreeNode<Integer>(15);
+    
+    root.right = new BinaryTreeNode<Integer>(29);
+    root.right.left = new BinaryTreeNode<Integer>(25);
+    root.right.right = new BinaryTreeNode<Integer>(34);
 
+    return root;
+}
 }
