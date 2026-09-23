817163c8c42556a1f29a85bd7f7718407905a8ea
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-09-23 16:16:45 -0700

    created two tests

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -1,6 +1,24 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
 
 public class BstSearchTest {
+
+    @Test 
+    public void testEmptyTree(){
+        assertFalse(BstSearch.contains(null, 10));
+    }
+
+    @Test 
+    public void testRootTarget(){
+        //arrange
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<> (10);
+
+        //assert
+        assertTrue(BstSearch.contains(root,10));
+    }
+
+
+  
 
 }
