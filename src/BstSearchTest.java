25a6f486481350523f8194fe90348481c1cd920c
sodatax <h.nacanaynay@gmail.com>
2026-09-23 12:23:05 -0700

    tested for wrong target

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
     @Test
-    void testTraverseBSTHasData(){
+    void testTraverseBSTHasTarget(){
         BinaryTreeNode<Integer> root = new BinaryTreeNode<>(50);
 
         root.left = new BinaryTreeNode<>(28);
@@ -16,5 +16,21 @@ public class BstSearchTest {
         assertTrue(BstSearch.contains(root, 28));
         assertTrue(BstSearch.contains(root, 10));
         assertTrue(BstSearch.contains(root, 48));
+    }
+
+    @Test
+    void testTraverseBSTHasWrongTarget(){
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(50);
+
+        root.left = new BinaryTreeNode<>(28);
+        root.right = new BinaryTreeNode<>(72);
+        root.left.left = new BinaryTreeNode<>(10);
+        root.left.right = new BinaryTreeNode<>(48);
+        root.right.left = new BinaryTreeNode<>(60);
+        root.right.right = new BinaryTreeNode<>(84);
+
+        assertFalse(BstSearch.contains(root, 11));
+        assertFalse(BstSearch.contains(root, 39));
+        assertFalse(BstSearch.contains(root, 30));
     }
 }
