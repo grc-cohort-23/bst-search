096308b6560b4f43ab5cd8c145565b462f224a2b
sodatax <h.nacanaynay@gmail.com>
2026-09-23 12:19:25 -0700

    fixed the traversal and created a test for values in the tree

--- src/BstSearch.java
+++ src/BstSearch.java
@@ -24,8 +24,9 @@ public class BstSearch {
     public static <T extends Comparable<T>> boolean contains(BinaryTreeNode<T> root, T target) {
         // Implement here, and make sure to implement tests too!
         if(target==null) throw new NullPointerException();
-        if(target.compareTo(root.data) <= 0) contains(root.left, target);
-        if(target == root.data) return true;
-        return false;
+        if(root == null) return false;
+        if(target.compareTo(root.data) == 0) return true;
+        else if(target.compareTo(root.data) < 0) return contains(root.left, target);
+        else return contains(root.right, target);
     }
 }
--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -3,5 +3,18 @@ import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
     @Test
-    
+    void testTraverseBSTHasData(){
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(50);
+
+        root.left = new BinaryTreeNode<>(28);
+        root.right = new BinaryTreeNode<>(72);
+        root.left.left = new BinaryTreeNode<>(10);
+        root.left.right = new BinaryTreeNode<>(48);
+        root.right.left = new BinaryTreeNode<>(60);
+        root.right.right = new BinaryTreeNode<>(84);
+
+        assertTrue(BstSearch.contains(root, 28));
+        assertTrue(BstSearch.contains(root, 10));
+        assertTrue(BstSearch.contains(root, 48));
+    }
 }
