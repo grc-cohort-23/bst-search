fab47e25933c648281f138e63a153b19557ee8f8
onjamm <azmionjang@gmail.com>
2026-09-23 14:10:40 -0700

    created test tree as well as test case, however, contains method seems to be flawed

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,9 +2,24 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
-//arrange 
 
-//act 
+    @Test 
+    public void testContains15() {
+        assertTrue(BstSearch.contains(testTree(), 15));
+    }
+    private BinaryTreeNode<Integer> testTree() {
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(23);
+        root.left = new BinaryTreeNode<Integer>(13);
+        root.left.left = new BinaryTreeNode<Integer>(7);
+        root.left.right = new BinaryTreeNode<Integer>(15);
 
-//assert
+        root.right = new BinaryTreeNode<Integer>(29);
+        root.right.left = new BinaryTreeNode<Integer>(25);
+        root.right.right = new BinaryTreeNode<Integer>(34);
+
+        return root;
 }
+}
+
+
+
