080361ccd4dd93385420cf24094397d0b86e932b
Jessica H <jessicahebert75@gmail.com>
2026-09-23 23:34:53 -0700

    Test BST root search

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,5 +2,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
+    @Test
+    void containsRoot() {
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(8);
 
+        assertTrue(BstSearch.contains(root, 8));
+    }
 }
