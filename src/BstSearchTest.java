c3a1e18a291ddf564ab3accccf6d45f357bdf559
AaronB22 <aaronthesnowboarder@live.com>
2026-09-23 21:03:58 -0700

    added true test

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -2,5 +2,15 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
-
+   
+    @Test 
+    void contains_ints(){
+        BinaryTreeNode<Integer> root= new BinaryTreeNode<Integer>(25, 
+            new BinaryTreeNode<Integer>(5, 
+            new BinaryTreeNode<Integer>(2), new BinaryTreeNode<Integer>(20)),
+             new BinaryTreeNode<>(30,
+                new BinaryTreeNode<>(27), new BinaryTreeNode<>(42)
+             ));
+             assertEquals(true, BstSearch.contains(root, 27));;
+    }
 }
