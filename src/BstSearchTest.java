aa2978e0e2d92a77cf3c8638f039a1a67cc74797
elena5100 <194572363+elena5100@users.noreply.github.com>
2026-09-24 00:05:48 -0700

    Add BST tests

--- src/BstSearchTest.java
+++ src/BstSearchTest.java
@@ -3,4 +3,18 @@ import org.junit.jupiter.api.Test;
 
 public class BstSearchTest {
 
+    @Test
+    public void testFound() {
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(10);
+        root.left = new BinaryTreeNode<>(5);
+
+        assertTrue(BstSearch.contains(root, 5));
+    }
+
+    @Test
+    public void testNotFound() {
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(10);
+
+        assertFalse(BstSearch.contains(root, 20));
+    }
 }
